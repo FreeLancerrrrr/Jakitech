a86fca730d0f122729b5bd5ad92fb828715375af
Jiannpeng <jiannpeng2000.com>
2022-10-14 23:54:43 +0800

    size

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -43,15 +43,15 @@ export const Skills = () => {
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>App Development</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>UI Design</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>UX Design</h5>
                             </div>
                         </Carousel>
                     </div>

95a2f2790caba94a73d955d798bbe4e4aad78fb8
Khong06 <76540922+Khong06@users.noreply.github.com>
2022-10-16 02:18:36 +0100

    video

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,11 +1,12 @@
 import { Col } from "react-bootstrap";
 import VideoPlayer from "react-video-js-player";
+import "../Video.css"
 
 export const ProjectCard = ({ title, description, video }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-videobx">
-        <VideoPlayer src={video} />
+        <VideoPlayer src={video} playbackRates={[1,1.5,2,2.5]}/>
         
         <div className="proj-txtx">
           <h4>{title}</h4>

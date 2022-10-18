46f52546752255b480925fcd012716667faa8fc3
Jiannpeng <jiannpeng2000.com>
2022-10-18 16:31:37 +0800

    video

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,18 +1,17 @@
 import { Col } from "react-bootstrap";
-import VideoPlayer from "react-video-js-player";
-import "../Video.css"
 
-//*not sure should be here or not
-         //code: width="250" height="300" inside the video player*//
-         
 export const ProjectCard = ({ title, description, video }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      
       <div className="proj-videobx">
-         
-        <VideoPlayer src={video} playbackRates={[1,1.5,2,2.5]}/>
-        
+        <video
+            style={{ maxWidth: "100%", width: "800px", margin: "0 auto" }}
+            playsInline
+            autoPlay
+            loop
+            muted
+            controls
+            src={video} />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,9 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import video1 from "../assets/Video/Poster.mp4"
-import video2 from "../assets/Video/Poster.mp4"
-import video3 from "../assets/Video/Poster.mp4"
+import video1 from "../assets/video/iu.mp4"
+import video2 from "../assets/video/iu.mp4"
+import video3 from "../assets/video/iu.mp4"
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 

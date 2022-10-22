0dbabcff901d604b01a78610e11cc42020a3fce1
Jiannpeng <jiannpeng2000.com>
2022-10-23 05:14:47 +0800

    cardview

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,16 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
+import { CardView } from "./CardView";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import video1 from "../assets/video/iu.mp4"
-import video2 from "../assets/video/iu.mp4"
+import video1 from "../assets/video/TaskManagement.mp4"
+import video2 from "../assets/video/PasswordStrength.mp4"
 import video3 from "../assets/video/iu.mp4"
 import video4 from "../assets/video/Forgot.mp4"
 import video5 from "../assets/video/Forgot.mp4"
 import video6 from "../assets/video/NavigationDrawerr.mp4"
+import img1 from "../assets/img/project-img1.png";
+import img2 from "../assets/img/project-img2.png";
+import img3 from "../assets/img/project-img3.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -15,13 +19,13 @@ export const Projects = () => {
   const projects = [
     {
       title: "Task Management App",
-      description: "by Lee Ji Eun",
-      video:video2,
+      description: "By Jiann Peng Lim",
+      video:video1,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      video:video1,
+      title: "Password Strength Checker",
+      description: "By Jiann Peng Lim",
+      video:video2,
     },
     {
       title: "Business Startup",
@@ -42,6 +46,72 @@ export const Projects = () => {
       title: "Business Startup",
       description: "Design & Development",
       video:video6,
+    },
+  ];
+
+  const appDev = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: img1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: img2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: img3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: img1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: img2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: img3,
+    },
+  ];
+
+  const uiDev = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: img1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: img2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: img3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: img1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: img2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: img3,
     },
   ];
 
@@ -82,11 +152,33 @@ export const Projects = () => {
                         }
                       </Row>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    <Tab.Pane eventKey="second">
+                      <Row>
+                          {
+                            appDev.map((project, index) => {
+                              return (
+                                <CardView
+                                  key={index}
+                                  {...project}
+                                  />
+                              )
+                            })
+                          }
+                      </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      <Row>
+                          {
+                            uiDev.map((project, index) => {
+                              return (
+                                <CardView
+                                  key={index}
+                                  {...project}
+                                  />
+                              )
+                            })
+                          }
+                      </Row>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
